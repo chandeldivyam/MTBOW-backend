9606eeeb932c0eca423391a5ef94b71642db5da1
Divyam Chandel <chandeldivyam@gmail.com>
2022-10-10 21:07:49 +0530

    crom time management, no participation after event start

--- db/postgresql-config.js
+++ db/postgresql-config.js
@@ -1,10 +1,12 @@
 const postgresql_config = {
     user: "doadmin",
-    host: "private-db-postgresql-mtbow-do-user-12616070-0.b.db.ondigitalocean.com",
+    host: "db-postgresql-mtbow-do-user-12616070-0.b.db.ondigitalocean.com",
     port: 25060,
     database: "mtbow_prod",
     password: "AVNS_1IGW3bG944tV2RY6MLq",
     ssl: {
-	rejectUnauthorized: false}};
+        rejectUnauthorized: false,
+    },
+};
 
 module.exports = postgresql_config;
